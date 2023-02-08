d156e97a2348191cfe312ea2c9a5c106464423b8
Nidhi <nidhiparabmail@gmail.com>
2023-02-08 14:11:42 +0530

    server v2

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.post('/', async (req, res) => {
 
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send(error || 'Something went wrong!');
   }
 })
 
